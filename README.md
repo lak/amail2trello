a7598822ea1c19a52f5d54c0a4bea1cd3a3874c3
Luke Kanies <luke@puppetlabs.com>
2015-01-11 20:08:02 -0800

    First working version of a heavily refactored ruby script
    Signed-off-by: Luke Kanies <luke@puppetlabs.com>

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # amail2trello
-Send your selected Apple Mail messages to Trello
+Send your selected Apple Mail messages to Trello using Applescript and Ruby.
 
-__Disclaimer: This is ridiculous.__
+__Disclaimer: This is just getting started.__
 
 # Setup
 
-1. Copy amail2trello.rb to `~/bin`.
-2. Copy `amail2trello.applescript` to where you want to invoke it from.
-3. Edit `amail2trello.rb` following the guidance in the file.
-4. Select some messages.
-5. Run `amail2trello.applescript`.
+0. Install ruby-trello with 'rubygem install ruby-trello'
+1. Install files with 'make install'
+  # copies amail2trell.rb to ~/bin and makes it executable
+  # copies amail2trello.applescript to ~/Library/Scripts/Applications/Mail
+2. Select some messages.
+3. Run `amail2trello.applescript`.
--- amail2trello.rb
+++ amail2trello.rb
@@ -3,39 +3,83 @@
 require 'trello'
 include Trello
 
+CONFIG_FILE = File.expand_path("~/.trello.conf")
+
+class TrelloConfig
+
+  # Step 1. Get Your Application Key from here:
+  # https://trello.com/1/appKey/generate
+
+  # Step 2. Get Your Token to Approve Your "App" from here:
+  # https://trello.com/1/authorize?key=YOUR_APPLICATION_KEY_FROM_STEP_1&name=SOME_ARBITRARY_NAME&expiration=never&response_type=token&scope=read,write
+
+  # Step 3. Set up misc. variables
+
+  @params = ["public_key", "member_token", "board", "list", "user"]
+
+  def self.configure
+    config = self.new
+
+    Trello.configure do |trello_config|
+      trello_config.developer_public_key = config.public_key
+      trello_config.member_token =  config.member_token
+    end
+
+    config
+  end
+
+  def self.params
+    @params.dup
+  end
+
+  @params.each { |p| attr_accessor p }
+
+  def initialize
+    load_config(self)
+  end
+
+  def load_config(config)
+    unless File.exist?(CONFIG_FILE)
+      warn "You have to create a trello config file at #{CONFIG_FILE}"
+      warn "See the example file at <fill this in>"
+      exit(1)
+    end
+
+    File.readlines(CONFIG_FILE).each do |line|
+      line.chomp!
+      next if line =~ /^#/ # skip comments
+      next if line =~ /^\s*$/ # skip blank lines
+
+      key, value = line.split(/:\s+/) # foo: bar
+
+      unless key and value
+        raise "Invalid format '#{line}'; expecting 'key: value'"
+      end
+
+      begin
+        config.send(key + "=", value)
+      rescue
+        raise "Invalid parameter '#{key}'; supported params are '#{Config.params.join(", ")}'"
+      end
+    end
+  end
+end
+
+config = TrelloConfig.configure
+
 subject = ARGV[0]
 id = ARGV[1]
 message = ARGV[2]
 
-# There's a little setup involved here.
-
-# Step 1. Get Your Application Key from here:
-# https://trello.com/1/appKey/generate
-public_key = ""
-
-# Step 2. Get Your Token to Approve Your "App" from here:
-# https://trello.com/1/authorize?key=YOUR_APPLICATION_KEY_FROM_STEP_1&name=SOME_ARBITRARY_NAME&expiration=never&response_type=token&scope=read,write
-member_token = ""
-
-# Step 3. Set up misc. variables
-board_name = ""
-list_name = ""
-username = ""
-
-
-Trello.configure do |config|
-  config.developer_public_key = public_key
-  config.member_token =  member_token
-end
-
-user = Member.find(username)
-board = user.boards.select { |b| b.name == board_name }.first
-list = board.lists.select { |l| l.name == list_name }.first
+user = Member.find(config.user)
+board = user.boards.select { |b| b.name == config.board }.first
+list = board.lists.select { |l| l.name == config.list }.first
   
 message_id =  CGI.escape(id)
 
-card = Card.create(:name => subject, :desc => "message://%3c#{id}%3e\n\n#{message}", :list_id => list.id)
+card = Card.create(:name => subject, :desc => "message://%3c#{id}%3e\n\n#{message}", :list_id => list.id, :pos => "bottom")
+puts card.short_url
 
-
-
+# This will take you to the card's page
+system("open #{card.short_url}")
 
